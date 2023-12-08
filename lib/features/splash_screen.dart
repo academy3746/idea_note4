a3290026e998aeb3b7972e605a603c7bb7c71ec8
ijonghyeon <academy3746@gmail.com>
2023-12-08 18:50:09 +0900

    2nd Layout

--- lib/features/splash_screen.dart
+++ lib/features/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:idea_note4/constants/gaps.dart';
+import 'package:idea_note4/constants/sizes.dart';
+import 'package:idea_note4/features/main_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,7 +14,45 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.pushReplacementNamed(context, MainScreen.routeName);
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(
+          Sizes.size20,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/splash.png",
+                width: Sizes.size150 + Sizes.size30,
+                height: Sizes.size150 + Sizes.size30,
+              ),
+              Gaps.v32,
+              const Text(
+                "아이디어노트",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: Sizes.size40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
